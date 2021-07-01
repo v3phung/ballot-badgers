c3ebf928df9cdfcbe7363673520567cb8eea1ad4
David Masselink <david_masselink@intuit.com>
2021-07-01 15:30:07 -0700

    fixing logo ref

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,7 +1,7 @@
 export const PageHeader = ({ title, slogan }) => {
   return (
     <header className="header-block">
-      <img src="./badger.png" alt='badger logo' className="logo" />
+      <img src="/badger.png" alt='badger logo' className="logo" />
       <h1 className="page-header">{title}</h1>
       <span className="slogan">{slogan}</span>
     </header>
